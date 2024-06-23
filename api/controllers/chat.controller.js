137dfb9a0e9004bcdf7a7fd5c785d3820d366983
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-06-23 15:19:57 +0530

    added addmebers to Group api

--- api/controllers/chat.controller.js
+++ api/controllers/chat.controller.js
@@ -1,88 +1,131 @@
 import { ALERT, REFETCH_CHATS } from "../constants/events.js";
 import { getOtherMember } from "../lib/helper.js";
 import { Chat } from "../models/chat.model.js";
+import { User } from "../models/user.modedl.js";
+import { errorHandler } from "../utils/error.js";
 import { emitEvent } from "../utils/features.js";
 
 export const newGroupChat = async (req, res, next) => {
-    try {
-        const { name, members } = req.body;
+  try {
+    const { name, members } = req.body;
 
-        if (members.length < 2) {
-            return next(errorHandler(400, "At least 2 members are required"));
-        }
-
-        const allMembers = [...members, req.user];
-        await Chat.create({
-            name,
-            members: allMembers,
-            groupChat: true,
-            creator: req.user,
-        });
-        emitEvent(req, ALERT, allMembers, `Welcome to ${name} group`);
-        emitEvent(req, REFETCH_CHATS, members);
-
-        return res.status(201).json({
-            success: true,
-            message: `Welcome to ${name} group`
-        });
-    } catch (error) {
-        next(error);
+    if (members.length < 2) {
+      return next(errorHandler(400, "At least 2 members are required"));
     }
+
+    const allMembers = [...members, req.user];
+    await Chat.create({
+      name,
+      members: allMembers,
+      groupChat: true,
+      creator: req.user,
+    });
+    emitEvent(req, ALERT, allMembers, `Welcome to ${name} group`);
+    emitEvent(req, REFETCH_CHATS, members);
+
+    return res.status(201).json({
+      success: true,
+      message: `Welcome to ${name} group`,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const getMyChats = async (req, res, next) => {
-    try {
-        const chats = await Chat.find({ members: req.user }).populate("members", "name avatar");
-        const transformedChats = chats.map(({ _id, name, members, groupChat }) => {
-            const otherMember = getOtherMember(members, req.user);
-            return {
-                _id,
-                groupChat,
-                avatar: groupChat ? members.slice(0, 3).map(({ avatar }) => avatar.url) : [otherMember.avatar.url],
-                name: groupChat ? name : otherMember.name,
-                members:members.reduce((prev,curr)=>{
+  try {
+    const chats = await Chat.find({ members: req.user }).populate(
+      "members",
+      "name avatar"
+    );
+    const transformedChats = chats.map(({ _id, name, members, groupChat }) => {
+      const otherMember = getOtherMember(members, req.user);
+      return {
+        _id,
+        groupChat,
+        avatar: groupChat
+          ? members.slice(0, 3).map(({ avatar }) => avatar.url)
+          : [otherMember.avatar.url],
+        name: groupChat ? name : otherMember.name,
+        members: members.reduce((prev, curr) => {
+          if (curr._id.toString() !== req.user.toString()) {
+            prev.push(curr._id);
+          }
+          return prev;
+        }, []),
+      };
+    });
 
-                    if(curr._id.toString() !== req.user.toString()){
-                        prev.push(curr._id)
-                    }
-                    return prev;
-                },[]),
-            };
-
-           
-        });
-
-        res.status(200).json({
-            success: true,
-            chats: transformedChats
-        });
-    } catch (error) {
-        next(error);
-    }
+    res.status(200).json({
+      success: true,
+      chats: transformedChats,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
+export const getMyGroups = async (req, res, next) => {
+  try {
+    const chats = await Chat.find({
+      members: req.user,
+      groupChat: true,
+      creator: req.user,
+    }).populate("members", "name avatar");
 
-export const getMyGroups = async (req, res,next) => {
-    try {
-        const chats = await Chat.find({
-            members: req.user,
-            groupChat: true,
-            creator: req.user,
-        }).populate("members", "name avatar");
+    const groups = chats.map(({ members, _id, groupChat, name }) => ({
+      _id,
+      groupChat,
+      name,
+      avatar: members.slice(0, 3).map(({ avatar }) => avatar.url),
+    }));
 
-        const groups = chats.map(({ members, _id, groupChat, name }) => ({
-            _id,
-            groupChat,
-            name,
-            avatar: members.slice(0, 3).map(({ avatar }) => avatar.url),
-        }));
+    res.status(200).json({
+      success: true,
+      groups,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
-        res.status(200).json({
-            success: true,
-            groups,
-        });
-    } catch (error) {
-        next(error);
-        
+export const addMember = async (req, res, next) => {
+  try {
+    const { chatId, members } = req.body;
+  
+    if(!members || members.length < 1) { return next(errorHandler(400,"Please Provide Mmembers"))}
+    const chat = await Chat.findById(chatId);
+
+    if (!chat) {
+      return next(errorHandler(404, "Chat not Found"));
     }
-}
+    if (!chat.groupChat) {
+     return next(errorHandler(404, "Chat not Found"));
+    }
+
+    if (chat.creator.toString() !== chat.creator.toString()) {
+    return  next(errorHandler(403, "Your not Allowed to add membbers"));
+    }
+     
+    const allNewMembersPromises = members.map((i) => User.findById(i, "name"));
+
+
+    const allMembers = await Promise.all(allNewMembersPromises);
+
+    chat.members.push(...allMembers.map((i)=>i._id));
+    if(chat.members.length > 100) { return next(errorHandler(400, "Group Members limit reached"))}
+
+    await chat.save();
+
+    const allUserName = allMembers.map((i) =>i.name).join(",");
+
+      emitEvent(req,ALERT,chat.members,`${allUserName} has been added to ${chat.name} group by ${req.user.name}`);
+    res.status(200).json({
+        success:true,
+        message:"All Members added  succesfully"
+    })
+
+} catch (error) {
+    next(error);
+  }
+};
