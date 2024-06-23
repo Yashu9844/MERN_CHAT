62b2f981689b3dd1370ad5938efdfba9af996bbe
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-06-23 15:20:35 +0530

    addeMember api

--- api/routes/chat.route.js
+++ api/routes/chat.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getMyChats, getMyGroups, newGroupChat } from '../controllers/chat.controller.js';
+import { addMember, getMyChats, getMyGroups, newGroupChat } from '../controllers/chat.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -7,4 +7,5 @@ const router = express.Router();
 router.post("/new",verifyToken,newGroupChat)
 router.get("/my",verifyToken,getMyChats)
 router.get("/my/groups",verifyToken,getMyGroups)
+router.put("/addMembers",verifyToken,addMember)
 export default router
